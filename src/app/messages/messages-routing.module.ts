e350b717cef3d5472d2a0dff0d62691dc651458a
Don  Mark Cox <dmarkcox@yahoo.com>
2021-04-10 13:25:05 -0700

    orders and message lists

--- src/app/messages/messages-routing.module.ts
+++ src/app/messages/messages-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MessageListComponent } from 'src/app/messages/message-list/message-list.component';
+
+const routes: Routes = [
+  {path: '', component: MessageListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/orders/orders-routing.module.ts
+++ src/app/orders/orders-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { OrderListComponent } from 'src/app/orders/order-list/order-list.component';
 
+const routes: Routes = [
+  {path: '', component: OrderListComponent }
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
